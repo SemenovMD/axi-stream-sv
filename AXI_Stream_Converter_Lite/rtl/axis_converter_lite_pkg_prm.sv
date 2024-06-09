9859214d9a4184f9612439839d9cd05bbbce1a46
SemenovMD <makcsem64@gmail.com>
2024-06-09 12:24:11 -0600

    Added AXIS_Converter_Lite

---
+++ AXI_Stream_Converter_Lite/rtl/axis_converter_lite_pkg_prm.sv
@@ -0,0 +1,8 @@
+package axis_converter_lite_pkg_prm;
+
+    parameter AXI_DATA_WIDTH    = 32;
+    parameter AXI_ADDR_WIDTH    = 32;
+
+    parameter AXI_ADDR          = 32'h1000_0001;
+
+endpackage
---
+++ AXI_Stream_Converter_Lite/rtl/axis_if.sv
@@ -0,0 +1,23 @@
+interface axis_if;
+
+    import axis_converter_lite_pkg_prm::AXI_DATA_WIDTH;
+
+    logic   [AXI_DATA_WIDTH-1:0]    tdata;
+    logic                           tvalid;
+    logic                           tready;
+
+    modport m_axis
+    (
+        output tdata,
+        output tvalid,
+        input  tready
+    );
+
+    modport s_axis
+    (
+        input  tdata,
+        input  tvalid,
+        output tready
+    );
+
+endinterface
