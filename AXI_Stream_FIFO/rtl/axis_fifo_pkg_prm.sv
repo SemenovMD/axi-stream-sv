06a6386df77c0fd85e19679d0c86f9004602322f
SemenovMD <makcsem64@gmail.com>
2024-06-12 05:00:48 -0600

    Deleted AXIS_Stream_FIFO

--- AXI_Stream_FIFO/rtl/axis_fifo_pkg_prm.sv
+++
@@ -1,6 +0,0 @@
-package axis_fifo_pkg_prm;
-
-    parameter AXI_DATA_WIDTH = 32;
-    parameter AXI_DATA_DEPTH = 32;
-
-endpackage
--- AXI_Stream_FIFO/tb/axis_fifo_pkg_tb.sv
+++
@@ -1,8 +0,0 @@
-package axis_fifo_pkg_tb;
-
-    parameter AXI_TRAN_MIN_DELAY = 2;
-    parameter AXI_TRAN_MAX_DELAY = 17;
-
-    parameter AXI_TRAN_MAX_WAIT = 150;
-
-endpackage
